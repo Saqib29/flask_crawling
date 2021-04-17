11997136b08b78dfe21a737eadf82349da4cc8c9
saqib <saqib29111@gmail.com>
2021-04-17 23:34:55 +0600

    complete searched image fetching from google

--- googlesearch/script.py
+++ googlesearch/script.py
@@ -12,7 +12,7 @@ goog = Blueprint("script", __name__, template_folder="templates")
 
 @goog.route("/search")
 def search():
-    return render_template("index.html")
+    return render_template("index1.html")
 
 
 @goog.route("/result", methods=['POST'])
@@ -20,8 +20,8 @@ def getResult():
     search = request.form['s']
 
     if len(search) == 0:
-        flash("please type for search.")
-        return render_template("index.html")
+        flash("please type something for search.")
+        return render_template("index1.html")
 
     chromedriver = "/home/saqib/work/selenium/chromedriver/chromedriver"
     driver = webdriver.Chrome(chromedriver)
@@ -33,15 +33,32 @@ def getResult():
     driver.execute_script("arguments[0].click();", driver.find_element_by_name("btnK"))
 
     driver.maximize_window()
-    # driver.refresh()
-    driver.execute_script("window.scrollTo(0,200);")
+    driver.refresh()
+
+    image = driver.find_element_by_xpath('//*[@id="hdtb-msb"]/div[1]/div/div[2]/a')
+    image.click()
 
 
-    
-    results = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='g']")))
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,2000);")
+    time.sleep(1)
 
+
+    imgs = driver.find_elements_by_tag_name('img')
+
+    images = [img.get_attribute('src') for img in imgs]
+
+    srcs = []
+
+    for image in images:
+        if image is None:
+            continue
+        if "http" not in image:
+            continue
+        
+        srcs.append(image)
 
     time.sleep(4)
-    # driver.quit()
+    driver.quit()
 
-    return render_template('index.html')
+    return render_template("result.html", images=srcs)
